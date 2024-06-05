fb85ea5d2b6e1e755726d317b49f98b1fb4f9d7b
David Kim <David.Kim@flexigroup.com.au>
2024-06-05 14:30:51 +1000

    added initial files

---
+++ HGServer/CaptureTheFlag.cpp
@@ -0,0 +1,16 @@
+#include "CaptureTheFlag.h"
+
+extern void PutLogList(char* cMsg);
+extern char G_cTxt[512];
+extern char	G_cData50000[50000];
+extern void PutLogFileList(char* cStr);
+extern void PutAdminLogFileList(char* cStr);
+extern void PutItemLogFileList(char* cStr);
+extern void PutLogEventFileList(char* cStr);
+extern void PutHackLogFileList(char* cStr);
+extern void PutPvPLogFileList(char* cStr);
+extern FILE* pLogFile;
+extern HWND	G_hWnd;
+
+#pragma warning (disable : 4996 6011 6001 4244 4018 6385 6386 26451 6054 4267 6053 6031)
+
